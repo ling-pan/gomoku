4e1ca051e07ffc54854bb4696edbed679acb82c6
ling-pan <penny.ling.pan@gmail.com>
2017-04-08 11:52:30 +0800

    current player's and opponent player's feature planes combo

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.callbacks import CSVLogger
 from keras import backend as K
+from rotation_and_reflection import *
 
 BOARD_SIZE = 20
 EMPTY = 0
@@ -324,11 +325,12 @@ def get_micro_feature_planes(macro_feature_plane):
 	# todo: split the 2D numpy matrix macro_feature_plane into multiple binary micro_feature_planes
 	return True
 
-def extract_feature_planes(data_list):
-	# format of data_list: [{'state': state, 'action': action, 'winner': winner}, ...], ... [{'state': state, 'action': action, 'winner': winner}]
+def extract_feature_planes(data_list=[]):
+	# format of data_list: [[{'state': state, 'action': action, 'winner': winner}, ...], ... [{'state': state, 'action': action, 'winner': winner}]]
 	
-	# data_list = []
-	# data_list.append(conversion('/Users/panling/Desktop/gomoku/gomoku_dataset/opening_1_380/0x1-25(1).psq'))
+	# for debugging
+	data_list = []
+	data_list.append(conversion('/Users/panling/Desktop/gomoku/gomoku_dataset/opening_1_380/0x1-25(1).psq'))
 
 	macro_feature_plane_list = []
 	cnt = 0
@@ -341,30 +343,48 @@ def extract_feature_planes(data_list):
 		for record in game_record:
 			state, action, winner = record['state'], record['action'], record['winner']
 
+			if winner == 1:
+				loser = 2
+			else:
+				loser = 1
+
 			# get initial macro feature planes
 			if start:
 				macro_feature_plane = get_macro_feature_plane(state, winner)
+				macro_feature_plane_loser = get_macro_feature_plane(state, loser)
 				start = False
 			# update macro feature planes: state_prev -> current player's move, opponent player's move -> state_curr
 			else:
 				macro_feature_plane_tmp = update_macro_feature_plane(state, macro_feature_plane_prev, winner, move1_id)
+				macro_feature_plane_tmp_loser = update_macro_feature_plane(state, macro_feature_plane_prev_loser, loser, move1_id)
 				if move2_id != -1:
 					macro_feature_plane = update_macro_feature_plane(state, macro_feature_plane_tmp, winner, move2_id)
+					macro_feature_plane_loser = update_macro_feature_plane(state, macro_feature_plane_tmp_loser, loser, move2_id)
 				else:
 					macro_feature_plane = copy.deepcopy(macro_feature_plane_tmp)
+					macro_feature_plane_loser = copy.deepcopy(macro_feature_plane_tmp_loser)
 			
 			macro_feature_plane_prev = copy.deepcopy(macro_feature_plane)
+			macro_feature_plane_prev_loser = copy.deepcopy(macro_feature_plane_loser)
+
+			macro_feature_plane_combo = []
+			macro_feature_plane_combo.append(macro_feature_plane_prev)
+			macro_feature_plane_combo.append(macro_feature_plane_prev_loser)
+			macro_feature_plane_combo = np.array(macro_feature_plane_combo)
+
 			move1_id = action
 			move2_id = record['transition_move']
 
 			# raw macro feature plane
-			curr_macro_feature_plane = {'state': macro_feature_plane_prev, 'action': action}
+			curr_macro_feature_plane = {'state': macro_feature_plane_combo, 'action': action}
 			macro_feature_plane_list.append(curr_macro_feature_plane)
 			# split macro feature plane into micro feature planes
 			# micro_feature_planes = get_micro_feature_planes(macro_feature_plane)
 			
 			# excapsulate into BOARD_SIZE * BOARD_SIZE * FEATURE_CNT 3d units
 
+			print_np_matirx(state)
+			print_np_matirx(macro_feature_plane_prev)
 	# return value
 	return macro_feature_plane_list
 
@@ -402,141 +422,6 @@ def next_move_pos_id_conversion(next_move_row, next_move_col):
 	next_move_id = next_move_row * BOARD_SIZE + next_move_col
 	return next_move_id
 
-def rotate_90(board, next_move):
-	board_rotate_90 = np.zeros((BOARD_SIZE, BOARD_SIZE))
-	
-	n = BOARD_SIZE
-
-	for i in range(n):
-		for j in range(n):
-			board_rotate_90[j][n - 1 - i] = board[i][j]
-
-	next_move_i, next_move_j = next_move_id_pos_conversion(next_move)
-	next_move_rotate_90 = next_move_pos_id_conversion(next_move_i, next_move_j)
-
-	return board_rotate_90, next_move_rotate_90
-
-def rotate_180(board, next_move):
-	board_rotate_180 = np.zeros((BOARD_SIZE, BOARD_SIZE))
-	
-	n = BOARD_SIZE
-
-	for i in range(n):
-		for j in range(n):
-			board_rotate_180[n - 1 - i][n - 1 - j] = board[i][j]
-
-	next_move_i, next_move_j = next_move_id_pos_conversion(next_move)
-	next_move_rotate_180 = next_move_pos_id_conversion(next_move_i, next_move_j)
-
-	return board_rotate_180, next_move_rotate_180
-
-def rotate_270(board, next_move):
-	board_rotate_270 = np.zeros((BOARD_SIZE, BOARD_SIZE))
-	
-	n = BOARD_SIZE
-
-	for i in range(n):
-		for j in range(n):
-			board_rotate_270[n - 1 - j][i] = board[i][j]
-
-	next_move_i, next_move_j = next_move_id_pos_conversion(next_move)
-	next_move_rotate_270 = next_move_pos_id_conversion(next_move_i, next_move_j)
-
-	return board_rotate_270, next_move_rotate_270
-
-def mirror_y_axis(board, next_move):
-	board_mirror_y = np.zeros((BOARD_SIZE, BOARD_SIZE))
-	
-	n = BOARD_SIZE
-
-	for i in range(n):
-		for j in range(n):
-			board_mirror_y[i][n - 1 - j] = board[i][j]
-
-	next_move_i, next_move_j = next_move_id_pos_conversion(next_move)
-	next_move_mirror_y = next_move_pos_id_conversion(next_move_i, next_move_j)
-
-	return board_mirror_y, next_move_mirror_y
-
-def mirror_x_axis(board, next_move):
-	board_mirror_x = np.zeros((BOARD_SIZE, BOARD_SIZE))
-	
-	n = BOARD_SIZE
-
-	for i in range(n):
-		for j in range(n):
-			board_mirror_x[n - 1 - i][j] = board[i][j]
-
-	next_move_i, next_move_j = next_move_id_pos_conversion(next_move)
-	next_move_mirror_x = next_move_pos_id_conversion(next_move_i, next_move_j)
-
-	return board_mirror_x, next_move_mirror_x
-
-# rotate the board in 4 directions and mirror the board in 2 directions to get additional 8 transformations
-def rotate_and_mirror(x_train, y_train):
-	appended_x_train, appended_y_train = [], []
-
-	num_of_training = len(y_train)
-	for i in range(num_of_training):
-		print 'data '+str(i)
-		# type 1: original with mirroring on x and y axis
-		# 0. original board
-		#print 'original...'
-		board, next_move = x_train[i], y_train[i]
-		appended_x_train.append(board)
-		appended_y_train.append(next_move)
-		# 1. mirror on x-axis
-		#print 'mirror on x-axis...'
-		board_mirror_x, next_move_mirror_x = mirror_x_axis(board, next_move)
-		appended_x_train.append(board_mirror_x)
-		appended_y_train.append(next_move_mirror_x)
-		# 2. mirror on y-axis
-		#print 'mirror on y-axis...'
-		board_mirror_y, next_move_mirror_y = mirror_y_axis(board, next_move)
-		appended_x_train.append(board_mirror_y)
-		appended_y_train.append(next_move_mirror_y)
-
-		# type 2: 90 degrees rotation with mirroring on x and y axis
-		# 3. roate 90 degrees
-		#print 'rotate 90 degrees...'
-		board_rotate_90, next_move_rotate_90 = rotate_90(board, next_move)
-		appended_x_train.append(board_rotate_90)
-		appended_y_train.append(next_move_rotate_90)
-		# 4. mirror on x-axis after rotating 90 degrees
-		#print 'rotate 90 degrees & mirror on x-axis...'
-		board_rotate_90_mirror_x, next_move_rotate_90_mirror_x = mirror_x_axis(board_rotate_90, next_move_rotate_90)
-		appended_x_train.append(board_rotate_90_mirror_x)
-		appended_y_train.append(next_move_rotate_90_mirror_x)
-		# 5. mirror on y-axis after rotating 90 degrees
-		#print 'rotate 90 degrees & mirror on y-axis...'
-		board_rotate_90_mirror_y, next_move_rotate_90_mirror_y = mirror_y_axis(board_rotate_90, next_move_rotate_90)
-		appended_x_train.append(board_rotate_90_mirror_y)
-		appended_y_train.append(next_move_rotate_90_mirror_y)
-
-		# type 3: 180 degrees rotation with mirroring on x axis
-		# 6. rotate 180 degrees
-		#print 'rotate 180 degrees...'
-		board_rotate_180, next_move_rotate_180 = rotate_180(board, next_move)
-		appended_x_train.append(board_rotate_180)
-		appended_y_train.append(next_move_rotate_180)
-		# 7. mirror on x-axis after rotating 180 degrees
-		#print 'rotate 180 degrees & mirror on x-axis...'
-		board_rotate_180_mirror_x, next_move_roate_180_mirror_x = mirror_x_axis(board_rotate_180, next_move_rotate_180)
-		appended_x_train.append(board_rotate_180_mirror_x)
-		appended_y_train.append(next_move_roate_180_mirror_x)
-
-		# type 4: 270 degrees rotation
-		# 8. rotate 270 degrees 
-		#print 'rotate 270 degrees...'
-		board_rotate_270, next_move_rotate_270 = rotate_270(board, next_move)
-		appended_x_train.append(board_rotate_270)
-		appended_y_train.append(next_move_rotate_270)
-
-	appended_x_train = np.array(appended_x_train)
-	appended_y_train = np.array(appended_y_train)
-
-	return appended_x_train, appended_y_train
-
 def shuffle_data(data_list):
 	return random.shuffle(data_list)
 
@@ -552,9 +437,10 @@ def sl_training(data_list):
 	# rotate_and_mirror(x_train, y_train)
 
 	# preprocessing
-	x_train = x_train.reshape(x_train.shape[0], BOARD_SIZE, BOARD_SIZE, 1)
-	x_test = x_test.reshape(x_test.shape[0], BOARD_SIZE, BOARD_SIZE, 1)
-	input_shape = (BOARD_SIZE, BOARD_SIZE, 1)
+	channels_cnt = 2
+	x_train = x_train.reshape(x_train.shape[0], BOARD_SIZE, BOARD_SIZE, channels_cnt)
+	x_test = x_test.reshape(x_test.shape[0], BOARD_SIZE, BOARD_SIZE, channels_cnt)
+	input_shape = (BOARD_SIZE, BOARD_SIZE, channels_cnt)
 
 	x_train = x_train.astype('float32')
 	x_test = x_test.astype('float32')
